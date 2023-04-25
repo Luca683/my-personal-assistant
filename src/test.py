e321cc5dc61f86c5c7305c6673770cb5d061afe9
Domenico Agatino Di Stefano <34487359+oromis34@users.noreply.github.com>
2023-04-25 13:33:30 +0200

    resolved review

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-stringa = "Voglio avere informazioni su cpu, ram e batteria"
-pattern = r"(?=.*\bcpu\b|\bram\b|\bbatteria\b)(?P<cpu>\bcpu\b)?(?P<ram>\bram\b)?(?P<batteria>\bbatteria\b)?"
-
-match = re.search(pattern, stringa)
-
-if match:
-    print(match.groupdict())
