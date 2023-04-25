1f1e7a85c3abe162890a4958a3ee95d31d97aff3
Domenico Agatino Di Stefano <34487359+oromis34@users.noreply.github.com>
2023-04-25 13:36:15 +0200

    fix: test definition

--- tests/test_module_benchmark.py
+++ tests/test_module_benchmark.py
@@ -14,13 +14,13 @@ from src.mod_benchmark import ModuleBenchmark
 functionalities_tests: List[dict] = [
     {"command": "", "check_command_res": False},
     {"command": "dammi i benchmark", "check_command_res": False},
-    {"command": "sistema e cpu", "check_command_res": False},
+    {"command": "sistema e processore", "check_command_res": False},
     {"command": "dammi i benchmark del sistema", "check_command_res": True, 
      "hasCpu": True, "hasRam": True, "hasBattery": True,
      "result": "La percentuale di utilizzo della CPU è del 3.3 percento, "
      + "la percentuale di utilizzo della RAM è del 55.4 percento, "
      + "che ammonta a 3.51 gigabyte utilizzati, la batteria è al 13.0 percento, e non è alimentata"},
-    {"command": "dimmi i benchmark della cpu", "check_command_res": True, 
+    {"command": "dimmi i benchmark della processore", "check_command_res": True,
      "hasCpu": True, "hasRam": False, "hasBattery": False,
      "result": "La percentuale di utilizzo della CPU è del 3.3 percento, "},
     {"command": "dammi le prestazioni della ram", "check_command_res": True, 
@@ -28,8 +28,9 @@ functionalities_tests: List[dict] = [
      "result": "la percentuale di utilizzo della RAM è del 55.4 percento, "
      + "che ammonta a 3.51 gigabyte utilizzati, "},
     {"command": "dammi le prestazioni della ram e della batteria", "check_command_res": True, 
-     "hasCpu": False, "hasRam": False, "hasBattery": True,
-     "result": "la batteria è al 13.0 percento, e non è alimentata"}
+     "hasCpu": False, "hasRam": True, "hasBattery": True,
+     "result": "la percentuale di utilizzo della RAM è del 55.4 percento, "
+     + "che ammonta a 3.51 gigabyte utilizzati, la batteria è al 13.0 percento, e non è alimentata"}
 ]
 
 
