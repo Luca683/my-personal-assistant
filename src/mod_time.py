15a97323e59de1cb6fc04c12c9d215604e6ef3ef
Spongix12 <116469328+Spongix12@users.noreply.github.com>
2023-05-21 19:06:16 +0200

    FinalFix

--- src/mod_time.py
+++ src/mod_time.py
@@ -4,6 +4,14 @@ import importlib
 
 master_module = importlib.import_module('master_module')
 
+def convert_month(month: int) -> str:
+    array_months = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"]
+    return array_months[month-1]
+
+def convert_week_day(day: str) -> str:
+    array_english_days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    array_italian_days = ["LunedÃ¬", "MartedÃ¬", "MercoledÃ¬", "GiovedÃ¬", "VenerdÃ¬", "Sabato", "Domenica"]
+    return array_italian_days[array_english_days.index(day)]
 
 class ModuleTime(master_module.MasterModule):
     def __init__(self) -> None:
@@ -24,23 +32,21 @@ class ModuleTime(master_module.MasterModule):
                 print(self.time)
             return True
         return False
-
+    
     def execute(self, command: str) -> str:
         print(command)
+        result = ''
+        now = datetime.datetime.now() # now Ã¨ una stringa con giorno, data e ora di oggi
         if self.time:
-            now = datetime.datetime.now()
-            current_time = now.strftime("Sono le : %H:%M")
-            if self.day:
-                d = datetime.datetime.now().strftime(" ed oggi è :%A")
-                current_time += d
-            return current_time
+            current_time = now.strftime("Sono le: %H:%M ")
+            result += current_time
         if self.day:
-            day = datetime.datetime.now().strftime("%A")
-            print(day)
-            return day
+            week_day = convert_week_day(now.strftime("%A"))
+            current_day = "Oggi Ã¨: " + week_day + " "
+            result += current_day
         if self.date:
-            date = datetime.datetime.now().strftime('Oggi è :%A, %B %d, %Y')
-            print(date)
-            return date
-        return "Specificami la data."
-   
+            month = convert_month(now.month)
+            current_date = now.strftime('Data: %d/'+month+'/%Y ')
+            result += current_date
+        print(result)
+        return result
